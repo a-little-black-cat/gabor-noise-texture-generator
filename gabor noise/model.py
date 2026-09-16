2acbd99eaa9f4473f096757a2d5f6af393a65bb8
a-little-black-cat <71386943+a-little-black-cat@users.noreply.github.com>
2026-09-15 20:16:18 -0400

    not yet implemented GAN

--- gabor noise/model.py
+++ gabor noise/model.py
@@ -1,4 +1,10 @@
-#train from a single source image, and generate an image with the same size as the source image
-
+# train from a single source image, and generate an image with the same size as the source image
+# inspired by: https://github.com/geomodeller/SinGAN_TF
+# https://www.tensorflow.org/tutorials/generative/dcgan
+import os
 import tensorflow as tf
 
+
+class Model:
+    def __init__(self):
+        pass
